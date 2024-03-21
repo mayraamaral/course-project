18206842fe4f702452d7a3db7eefc4d1e22fff4b
Mayra Amaral <mayracunha18@gmail.com>
2024-03-21 20:47:07 -0300

    feat: add size validation for username

--- src/main/java/dev/mayra/courses/entities/user/UserCreateDTO.java
+++ src/main/java/dev/mayra/courses/entities/user/UserCreateDTO.java
@@ -3,6 +3,8 @@ package dev.mayra.courses.entities.user;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -10,6 +12,8 @@ import lombok.Data;
 public class UserCreateDTO {
   @NotBlank(message = "The username can't be blank")
   @Schema(description = "Fill the username", required = true, example = "namelastname")
+  @Pattern(regexp = "^[a-z]+$", message = "The username must contain only lowercase characters, without numerals and spaces")
+  @Size(max = 20, message = "The username can have a maximum of 20 characters")
   private String username;
 
   @NotBlank(message = "The name can't be blank")
