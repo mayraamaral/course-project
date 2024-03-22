80111e96d7e669cfac45ca747a760e498dd4395a
Mayra Amaral <mayracunha18@gmail.com>
2024-03-22 20:37:51 -0300

    refactor: accept string as enum and return empty list if dont exists

--- src/main/java/dev/mayra/courses/app/services/CourseService.java
+++ src/main/java/dev/mayra/courses/app/services/CourseService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -29,17 +30,18 @@ public class CourseService {
   }
 
   public List<CourseResponseDTO> listAllOrByStatus(String status) {
-      CourseStatus statusEnum = null;
       try {
+        CourseStatus statusEnum = null;
 
         if(status != null) {
           statusEnum = CourseStatus.valueOf(status.toUpperCase());
         }
-        List<CourseResponseDTO> courses = courseRepository.findAllCoursesOrByStatus(statusEnum);
-        return courses;
 
+        List<CourseResponseDTO> courses = courseRepository.findAllCoursesOrByStatus(statusEnum);
+
+        return courses;
       } catch (IllegalArgumentException ex) {
-        throw new NotFoundException("Status " + status + " not found");
+        return new ArrayList<>();
       }
   }
 
